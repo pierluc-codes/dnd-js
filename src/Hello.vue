ef423eac75ca677176ea4c09a72019253febce10
Pier-Luc Caron St-Pierre <pier-luccaronst-pierre@motivateco.com>
2018-02-16 21:22:09 -0500

    Proto

--- src/Hello.vue
+++ src/Hello.vue
@@ -24,82 +24,40 @@
                 </tr>
             </tfoot>
             <tbody>
-                <tr>
-                    <td><img src="http://lorempixel.com/64/64/cats/6" /></td>
-                    <td>20</td>
-                    <td>Anomen</td>
-                    <td><a class="button is-success">Available</a></td>
-                    <td><a class="button is-info">Divine shield</a></td>
-                    <td><a class="button is-info">Faith armor</a></td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td><img src="http://lorempixel.com/64/64/cats/5" /></td>
-                    <td>19</td>
-                    <td>Lia</td>
-                    <td><a class="button is-success">Available</a></td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td><img src="http://lorempixel.com/64/64/cats/8" /></td>
-                    <td>17</td>
-                    <td>Gros zombie</td>
-                    <td><a class="button is-success">Available</a></td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td><img src="http://lorempixel.com/64/64/cats/7" /></td>
-                    <td>17</td>
-                    <td>Zombie qui court</td>
-                    <td><a class="button is-success">Available</a></td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-                <tr class="is-selected">
-                    <td><img src="http://lorempixel.com/64/64/cats/4" /></td>
-                    <td>17</td>
-                    <td>Jack</td>
-                    <td><a class="button is-success">Available</a></td>
-                    <td><a class="button is-danger">Poisoned</a></td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td><img src="http://lorempixel.com/64/64/cats/3" /></td>
-                    <td>16</td>
-                    <td>Alphonse</td>
-                    <td><a class="button is-success">Available</a></td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
-                </tr>
-                <tr>
-                    <td><img src="http://lorempixel.com/64/64/cats/2" /></td>
-                    <td>11</td>
-                    <td>Sigur</td>
-                    <td><a class="button is-danger">Unavailable</a></td>
+                <tr v-for="player in players" :key="player.id" v-bind:class="{ 'is-selected': player.active}" >
+                    <td><img :src="player.image" /></td>
+                    <td>{{player.initiative}}</td>
+                    <td>{{player.name}}</td>
+                    <td><font-awesome-icon :icon="player.reaction.icon" :style="player.reaction.style" /></td>
                     <td>
-                        <a class="button is-danger">Unconcious</a>
-                        <a class="button is-danger">Poisoned (2)</a>
-                        <a class="button is-danger">Paralyzed (10)</a>
-                        <a class="button is-info">Blessed</a>
+                        <div class="tags" v-if="player.conditions">
+                            <span class="tag" 
+                                v-bind:class="{ 'is-danger': condition.isPenalty, 'is-success': condition.isBonus }" 
+                                v-for="condition in player.conditions" :key="condition.id">
+                                {{ condition.name }}
+                            </span>
+                        </div>
+                        <div v-else>-</div>
                     </td>
-                    <td>-</td>
-                    <td>2/1</td>
-                </tr>
-                <tr>
-                    <td><img src="http://lorempixel.com/64/64/cats/1" /></td>
-                    <td>1</td>
-                    <td>Fargrim</td>
-                    <td><a class="button is-success">Available</a></td>
-                    <td>-</td>
-                    <td>-</td>
-                    <td>-</td>
+                    <td>
+                        <div span v-if="player.concentration">
+                            <span class="tag is-info">{{ player.concentration.name }}</span>
+                        </div>
+                        <div v-else>-</div>
+                    </td>
+                    <td>
+                        <div span v-if="player.deathSaving">
+                            <span>
+                                {{player.deathSaving.success}}
+                                <font-awesome-icon icon="thumbs-up" />
+                            </span>
+                            <span>
+                                {{player.deathSaving.failure}}
+                                <font-awesome-icon icon="thumbs-down" />
+                            </span>
+                        </div>
+                        <div v-else>-</div>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -107,11 +65,172 @@
 </template>
 
 <script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import { faCheck, faTimes } from '@fortawesome/fontawesome-free-solid'
+
 export default {
   data() {
     return {
-      a: "bobby"
+      players: [
+        {
+          "id": 0,
+          "initiative": 20,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Anomen",
+          "reaction": {
+              "icon": faCheck,
+              "style": {
+                  'color': 'green'
+              }
+          },
+          "conditions": [
+              {
+                  "id": "0_DivineShield",
+                  "name": "Divine shield",
+                  "isBonus": true
+              }
+          ],
+          "concentration": {
+            "name": "Faith armor"
+          }
+        },
+        {
+          "id": 0,
+          "initiative": 19,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Liaaaaa",
+          "reaction": {
+              "icon": faTimes,
+              "style": {
+                  'color': 'red'
+              }
+          },
+          "conditions": [
+          ] 
+        },
+        {
+          "id": 0,
+          "initiative": 18,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Gros zombie",
+          "reaction": {
+              "icon": faCheck,
+              "style": {
+                  'color': 'green'
+              }
+          },
+          "conditions": [
+          ] 
+        },
+        {
+          "id": 0,
+          "initiative": 17,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Zombie qui court",
+          "reaction": {
+              "icon": faCheck,
+              "style": {
+                  'color': 'green'
+              }
+          },
+          "conditions": [
+          ],
+        },
+        {
+          "id": 0,
+          "active": true,
+          "initiative": 16,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Jack",
+          "reaction": {
+              "icon": faCheck,
+              "style": {
+                  'color': 'green'
+              }
+          },
+          "conditions": [] 
+        },
+        {
+          "id": 0,
+          "initiative": 16,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Alphonse",
+          "reaction": {
+              "icon": faCheck,
+              "style": {
+                  'color': 'green'
+              }
+          },
+          "conditions": [
+              {
+                  "id": "0",
+                  "name": "Poisoned",
+                  "isPenalty": true
+              }
+          ] 
+        },
+        {
+          "id": 0,
+          "initiative": 11,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Sigur",
+          "reaction": {
+              "icon": faCheck,
+              "style": {
+                  'color': 'green'
+              }
+          },
+          "conditions": [
+              {
+                  "id": "0",
+                  "name": "Unconcious",
+                  "isPenalty": true
+              },
+              {
+                  "id": "0",
+                  "name": "Poisoned (2)",
+                  "isPenalty": true
+              },
+              {
+                  "id": "0",
+                  "name": "Paralyzed (33)",
+                  "isPenalty": true
+              },
+              {
+                  "id": "0",
+                  "name": "Blessed",
+                  "isBonus": true
+              },
+          ],
+          "deathSaving": {
+              "success": 2,
+              "failure": 1
+          } 
+        },
+        {
+          "id": 0,
+          "initiative": 1,
+          "image": "http://placekitten.com/g/64/64",
+          "name": "Fargrim",
+          "reaction": {
+              "icon": faCheck,
+              "style": {
+                  'color': 'green'
+              }
+          },
+          "conditions": [
+              {
+                  "id": "0",
+                  "name": "Poisoned",
+                  "isPenalty": true
+              }
+          ] 
+        }
+      ]
     };
+  },
+  components: {
+    FontAwesomeIcon
   }
 };
 </script>
